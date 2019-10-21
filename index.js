ed770f0a93dab52d0ab461be027d4aa69c049fd7
Charles Fries <charliefries@icloud.com>
2019-10-21 13:17:30 -0700

    Implemented contentFor hook

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+	name: require('./package').name,
+	contentFor(type, config) {
+		if (type === 'head') {
+			if (config.googleFonts) {
+				let families = config.googleFonts.join('|');
+				return '<link integrity="" rel="stylesheet" href="https://fonts.googleapis.com/css?family=' + families + '">';
+			}
+		}
+	}
 };
